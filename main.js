1d1e0791a5a77def929570401f9a78d3ee626b38
sumaris <alfred.radu@gmai.com>
2018-06-01 02:47:56 +0200

    added args

--- main.js
+++ main.js
@@ -10,30 +10,33 @@ client.login(token);
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
 
-    if (message.content.startsWith(`${prefix}ping`)) {
+    if (command === 'ping') {
         message.channel.send('Pong.');
     }
-    else if (message.content.startsWith(`${prefix}beep`)) {
+    else if (command === 'beep') {
         message.channel.send('Boop.');
     }
-    else if (message.content === `${prefix}server`) {
+    else if (command === 'server') {
         message.channel.send(`
         Server name: ${message.guild.name}
         Total members: ${message.guild.memberCount}
         Server region: ${message.guild.region}
         I was created: ${message.guild.createdAt}`);
     }
-    else if (message.content === `${prefix}user-info`) {
+    else if (command === 'user-info') {
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
     }
     else if (command === 'args-info') {
         if (!args.length) {
             return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
         }
-        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+
+        message.channel.send(`First argument: ${args[0]}`);
     }
 });
